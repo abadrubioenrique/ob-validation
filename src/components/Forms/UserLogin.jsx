8aa7af39d53a76ce5fb3365a87fcad3dea131d58
abadrubioenrique <abadrubioenrique@gmail.com>
2021-11-24 13:03:20 +0100

    Formik removed, token ready

--- src/components/Forms/UserLogin.jsx
+++ src/components/Forms/UserLogin.jsx
@@ -1,89 +1,74 @@
-import React, {useState} from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import '../Styles/userRegister.scss';
 
 const UserLogin = () => {
     const [formulario, setFormulario] = useState(false);
-    
-    const [datos, setDatos] = useState();    
+   
+    const [token, setToken] = useState('');
+    useEffect(() => {
+        if(token !== ''){
+            console.log("Token = " + token);
+        }
+        
+    },[token])
+    const [datos, setDatos] = useState(
+        {
+            data:[],
+            form:{
+                username:'',
+                password:''
+            }
+        }
+    );
     const ApiUrl="https://obvalid4.herokuapp.com/api/auth/login";
 
-    const peticionPost=async()=>{
-        await axios.post(ApiUrl,
-         {
-           username:datos.email,
-           password:datos.password
-         }
-        )}
-        
+    const peticionPost=()=>{
+        axios.post(ApiUrl,
+                {
+                    username:datos.form.username,
+                    password:datos.form.password
+                })
+                .then(response=>{
+                    setToken(response.data.token);
+                }).catch(error=>{
+                    alert("Hubo un error al loguear, intentelo de nuevo");
+                    })         
+
+        }
+       
+    const handleChange=async e=>{
+        e.persist();
+        await setDatos({
+            form:{
+            ...datos.form,
+            [e.target.name]: e.target.value  
+            }
+        });
+        }
+
+    const handleSubmit=async e=>{
+        e.preventDefault(setFormulario(false));
+        peticionPost();
+        }
+        const {form}=datos;
     return (
-        <Formik
-        initialValues={{
-            email:'',
-            password:''
-        }}
-        validate={(values) => {
-            let errors = {};
-
-            if (!values.email) {
-                errors.email = 'Por favor ingrese su e-mail';
-            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                errors.email = 'E-mail incorrecto';
-            }
-            if(!values.password){
-                errors.password = 'Por favor ingrese una contraseña'
-            } else if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(values.password)){
-                errors.password = 'La contraseña debe tener mínimo ocho caracteres, al menos una letra y un número:'
-            }
-
-            return errors;
-        }}
-        
-            onSubmit={(values, {resetForm}) => {
-            resetForm();
-            console.log('Formulario enviado');
-            alert(JSON.stringify(values, null, 2));
-            setDatos(values);
-            console.log("Bienvenido" + datos.username);
-            setFormulario(true);
-            peticionPost();
-            setTimeout(() => setFormulario(false), 3000);
-        }}
-    >
-        {( {errors} ) => (
             <>
             <h1>Login de Usuarios</h1>
-            <Form className="formulario shadow">
-                <div>
-                    <label htmlFor="email">E-mail</label>
-                    <Field
-                        type="email" 
-                        id="email" 
-                        name="email" 
-                        placeholder="E-mail"
-                    />
-                    <ErrorMessage name="email" component={() => (<div className="error">{errors.email}</div>)} />
-                </div>
-                <div>
-                    <label htmlFor="password">Contraseña</label>
-                    <Field
-                        type="password" 
-                        id="password" 
-                        name="password" 
-                        placeholder="Ingrese una contraseña"
-                    />
-                    <ErrorMessage name="password" component={() => (<div className="error">{errors.password}</div>)} />
-                </div>
+            <form onSubmit={handleSubmit} className="formulario shadow">
 
-                <button type="submit">Enviar</button>
-                {formulario && <p className="success">Formulario enviado con exito!</p>}
-            </Form>
+            <label htmlFor="id">Usuario</label>
+            <input className="form-control" maxLength="50" type="email" name="username" id="username" required onChange={handleChange} value={form?form.username: ''}/>
+
+            <label htmlFor="username">Contraseña</label>
+            <input className="form-control" type="password" name="password" id="password" required onChange={handleChange} value={form?form.password: ''}/>
+            <div className="enviar">
+            <button type="submit" className="btn btn-success" >Add</button>
+            {formulario && <p className="success">Form sent successfully!</p>}
+            </div>
+            </form>
             </>
         )}
 
-    </Formik>
-    );
-}
 
 export default UserLogin;
