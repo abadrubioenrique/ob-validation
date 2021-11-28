a467e794c67b69bcc931ef94ea0deb50a57b95d6
abadrubioenrique <abadrubioenrique@gmail.com>
2021-11-28 23:17:33 +0100

    css validation

--- src/components/Validation/FilepondComponent.jsx
+++ src/components/Validation/FilepondComponent.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect,useState } from 'react';
 import { FilePond } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
 import axios from 'axios';
 import { getToken } from '../../utils/helpers/auth-helpers';
 
-
+//Styles
+import 'filepond/dist/filepond.min.css';
+import './validation.scss'
 
  export const FilepondComponent = (props) => {
     const [files, setFiles] = useState([]);
@@ -36,7 +37,7 @@ import { getToken } from '../../utils/helpers/auth-helpers';
        
     }, []);
    return (
-     <div className="">
+     <div >
       <h1>Validación del Usuario</h1>
 
       
@@ -47,9 +48,10 @@ import { getToken } from '../../utils/helpers/auth-helpers';
         
         <h3 className="center">No ha sido validado</h3>
         <p className="center">Realice una fotografía de cada cara del dni</p>
-        <h1>Front</h1>
+        <h1>DNI - Cara frontal</h1>
+        <div className="subida">  
         <FilePond
-        name="frontal"
+        name="dni-front"
         required={true}
         allowMultiple={false}
         maxFiles={1}      
@@ -99,11 +101,63 @@ import { getToken } from '../../utils/helpers/auth-helpers';
         }
     }
     />
+</div>
+    <h1>DNI - Cara trasera</h1>
+    <div className="subida">
+        <FilePond
+        name="dni-back"
+        required={true}
+        allowMultiple={false}
+        maxFiles={1}      
+        server={
+        {
+            timeout: 99999,
+            process: (fieldName, file, metadata, load, error, progress, abort) => {
+
+                const formData = new FormData()
+                formData.append('multipartFile', file, file.name)
+
+                // aborting the request
+                const CancelToken = axios.CancelToken
+                const source = CancelToken.source()
+                axios({
+                    method: 'POST',
+                    url: API_URL + "/api/cloudinary/uploadback",
+                    data: formData,
+                    headers: {
+                        'content-type': 'multipart/form-data',
+                        'Authorization': `Basic ${token}` 
+                    },
+                    //cancelToken: source.token,
+                    onUploadProgress: (e) => {
+                        // updating progress indicator
+                        progress(e.lengthComputable, e.loaded, e.total)
+                    }
+                }).then(response => {
+                    // passing the file id to FilePond
+                    load(response.file)
+                }).catch((thrown) => {
+                    if (axios.isCancel(thrown)) {
+                        console.log('Request canceled', thrown.message)
+                    } else {
+                        // handle error
+                    }
+                })
+                // Setup abort interface
+                return {
+                    abort: () => {
+                        source.cancel('Operation canceled by the user.')
+                        abort()
+                    }
+                }
+            }
+
+        }
+    }
+    />
+    </div>
             </div>
 
-            
-            
-            
             )}
     </div>
 
