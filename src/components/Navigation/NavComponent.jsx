2fe3d95f2567c29275c4c6b3dcd7eaf4df2bffbe
abadrubioenrique <92922569+abadrubioenrique@users.noreply.github.com>
2021-12-01 22:03:38 +0100

    Update NavComponent.jsx

--- src/components/Navigation/NavComponent.jsx
+++ src/components/Navigation/NavComponent.jsx
@@ -1,76 +1,49 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { logout } from '../../store/slices/auth';
-import { removeUserInfo } from '../../store/slices/user';
-
-import './navigation.scss';
-const NavComponent = ({isLoggedIn}) => {
-    console.log(isLoggedIn);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    let userInfo = null;
-    userInfo = useSelector((state) => state.user.userInfo);
-    
-    const handleLogout = () => {
-      dispatch(logout());
-      dispatch(removeUserInfo());
-      navigate('/');
-    };
-
-
-    return ( 
-<div>
-    {userInfo != null ?
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
-            <div className="container-fluid ">
-                <Link className="navbar-brand mb-0 h1 text-light">
-                <div className="background-layer">
-                    <h2 className="logo validation">
-                    Check
-                    <span>Docs</span>
-                    </h2>
-                </div>
-                
-                </Link>
-                    <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex flex-row-reverse">
-                    <div class="navbar-nav">
-                <span className="nav-link username">
-                   Bienvenido {userInfo.name + " " + userInfo.surname}
-                </span>
-                    <button 
-                        className="nav-link btn logout" 
-                        onClick={ handleLogout }
-                    >
-                        LogOut
-                    </button>
-            </div>
-            </div>
-            </div>
-        </nav>
-               
-                :
+       <div>
+            {userInfo != null ?
                 <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
-            <div className="container-fluid">
+                <div className="container-fluid">
                 <div className="navbar-brand mb-0 h1 text-light" ><div className="background-layer">
                     <h2 className="logo">
                     Check
                     <span>Docs</span>
                     </h2>
-                </div></div>
-                    </div>
+                </div>
+                </div>
+                </div>
                     <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                    <NavLink  className={ ({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light' : '') } to="/register">
-                        Register</NavLink>
-                    <NavLink  className={ ({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light ' : '') } to="/login">
-                        Login</NavLink>
+                        <ul className="navbar-nav ml-auto">
+                            <span className="nav-item nav-link text-white-50">
+                                Bienvenido {userInfo.name + " " + userInfo.surname}
+                            </span>
+                            <button
+                                className="nav-item nav-link btn logout"
+                                onClick={handleLogout}
+                            >
+                                Logout
+                            </button>
+                        </ul>
+                    </div>
+                </nav>
+                :
+                <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
+                <div className="container-fluid">
+                <div className="navbar-brand mb-0 h1 text-light" ><div className="background-layer">
+                    <h2 className="logo">
+                    Check
+                    <span>Docs</span>
+                    </h2>
+                </div>
+                </div>
+                </div>
+                    <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                        <NavLink className={({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light' : '')} to="/register">
+                            Register</NavLink>
+                        <NavLink className={({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light ' : '')} to="/login">
+                            Login</NavLink>
 
 
-            </div>
-        </nav>
+                    </div>
+                </nav>
             }
-        
-</div>
-    );
-}
 
-export default NavComponent;
+        </div>
