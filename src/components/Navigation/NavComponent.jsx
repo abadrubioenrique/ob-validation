25dd6044cb3eaebeb22ad32077f3159f59dc1fb2
abadrubioenrique <92922569+abadrubioenrique@users.noreply.github.com>
2021-12-01 21:39:58 +0100

    Update NavComponent.jsx

--- src/components/Navigation/NavComponent.jsx
+++ src/components/Navigation/NavComponent.jsx
@@ -19,44 +19,57 @@ const NavComponent = ({isLoggedIn}) => {
 
 
     return ( 
-        <div>
-            {userInfo != null ?
-                <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand mb-0 h1 text-light validation" to="/">ValidationProject</Link>
-
-                    </div>
-                    <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                        <ul className="navbar-nav ml-auto">
-                            <span className="nav-item nav-link text-white-50">
-                                Bienvenido {userInfo.name + " " + userInfo.surname}
-                            </span>
-                            <button
-                                className="nav-item nav-link btn logout"
-                                onClick={handleLogout}
-                            >
-                                Logout
-                            </button>
-                        </ul>
-                    </div>
-                </nav>
+<div>
+    {(usuario!=null) && cargandoUsuario===false ?
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
+            <div className="container-fluid ">
+                <Link className="navbar-brand mb-0 h1 text-light validation" to ="/validation">
+                <div className="background-layer">
+                    <h2 className="logo validation">
+                    Check
+                    <span>Docs</span>
+                    </h2>
+                </div>
+                
+                </Link>
+                    <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex flex-row-reverse">
+                    <div class="navbar-nav">
+                <span className="nav-link username">
+                   Bienvenido {usuario.name + " " + usuario.surname}
+                </span>
+                    <button 
+                        className="nav-link btn logout" 
+                        onClick={ handleLogout }
+                    >
+                        LogOut
+                    </button>
+            </div>
+            </div>
+            </div>
+        </nav>
+               
                 :
                 <nav className="navbar navbar-expand-lg navbar-light bg-primary bg-gradient">
-                    <div className="container-fluid">
-                        <div className="navbar-brand mb-0 h1 text-light" >ValidationProject</div>
+            <div className="container-fluid">
+                <div className="navbar-brand mb-0 h1 text-light" ><div className="background-layer">
+                    <h2 className="logo">
+                    Check
+                    <span>Docs</span>
+                    </h2>
+                </div></div>
                     </div>
                     <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                        <NavLink className={({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light' : '')} to="/register">
-                            Register</NavLink>
-                        <NavLink className={({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light ' : '')} to="/login">
-                            Login</NavLink>
+                    <NavLink  className={ ({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light' : '') } to="/register">
+                        Register</NavLink>
+                    <NavLink  className={ ({ isActive }) => 'nav-item nav-link text-dark' + (isActive ? 'active text-light ' : '') } to="/login">
+                        Login</NavLink>
 
 
-                    </div>
-                </nav>
+            </div>
+        </nav>
             }
-
-        </div>
+        
+</div>
     );
 }
 
